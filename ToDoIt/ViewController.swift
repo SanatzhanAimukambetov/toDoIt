0fd1afc55744bf42484ef5473b1a4ec9e706490c
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-11-05 23:44:11 +0600

    first model of app

--- ToDoIt/ViewController.swift
+++ ToDoIt/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var newItem: Item?
+    
+    @IBOutlet var datePicker: UIDatePicker!
+    @IBOutlet var textField: UITextField!
+    
+    @IBOutlet var saveButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        saveButton.isEnabled = false
+        
+        textField.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
     }
+    
+    func saveItem() {
+        
+        newItem = Item(item: textField.text!, date: datePicker.date)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "save" {
+            let vc = segue.destination as! NewItemViewController
+            vc.label1 = textField.text
+            vc.label2 = datePicker.date
+        } else {
+            return
+        }
+    }
+}
 
-
+extension ViewController {
+    
+    @objc private func textFieldChanged() {
+        if textField.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
 }
 
---
+++ ToDoIt/model.swift
@@ -0,0 +1,16 @@
+//
+//  model.swift
+//  ToDoIt
+//
+//  Created by Kairat Zukhra on 05.11.2020.
+//
+
+import UIKit
+
+struct Item {
+    
+    var item: String
+    var date: Date
+}
+
+
